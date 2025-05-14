18af1ecd3284ffc9a201ac8bb3309d4088a272d6
Sean Mafnas <seanvincent671@gmail.com>
2025-05-14 22:07:13 +1000

    Add example objects for transaction and error handling

--- script.js
+++ script.js
@@ -2,4 +2,16 @@
 
 //data types & structures(string, number, boolean, array, object)
 
-const expenses = [1000, 30, 25];
+/* const expenses = [1000, 30, 25];
+const transaction = {
+  description: "Salary",
+  amount: 5000,
+};
+console.log(transaction.amount);
+
+const error = {
+  description: "Not Found",
+  statusCode: 404,
+  message: "The requested resource was not found",
+};
+ */
